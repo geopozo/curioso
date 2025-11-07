478d86dda91b25fa320d17f53ed029fad655ef54
David Angarita <davidangarita.sistemas@gmail.com>
2025-11-07 17:34:48 -0500

    Add print json in cli

--- src/curioso/_cli.py
+++ src/curioso/_cli.py
@@ -1,7 +1,15 @@
 import asyncio
+import json
 
 from curioso import app
 
+# ruff: noqa: T201 allow print in CLI
+
+
+async def main():
+    data = await app.probe()
+    print(json.dumps(data, indent=2, sort_keys=False))
+
 
 def run_cli():
-    asyncio.run(app.probe())
+    asyncio.run(main())
